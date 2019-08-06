aa186eb5de9745e5f0e200bd3fbc23f60d9f1c63
Nate Petersen <nate.petersen@me.com>
2019-08-06 19:40:38 +0000

    complete task 4 - first attempt

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,18 @@
-#!/usr/bin/env bash
+# change config line on server
+file_line { 'password':
+    ensure => present,
+    line => 'PasswordAuthentication no',
+    match => '^.?.*PasswordAuthentication yes$',
+    replace => true,
+    path => '/etc/ssh/ssh_config',
+    multiple => false,
+}
+
+file_line { 'identity':
+    ensure => present,
+    line => 'IdentityFile ~/.ssh/holberton',
+    match => '^.*IdentityFile ~/.ssh/holberton$',
+    path => '/etc/ssh/ssh_config',
+    multiple => false,
+    append_on_no_match => true,
+}
