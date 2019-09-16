066b58282650feba52aa2997b07e923c4c9ceeea
Nate Petersen <nate.petersen@me.com>
2019-09-16 20:48:44 +0000

    add 'if name == main'

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,25 +4,26 @@
 import requests
 import sys
 
+if __name__ == "__main__":
+    id = sys.argv[1]
+    info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        id))
+    todo = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
 
-id = sys.argv[1]
-info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(id))
-todo = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
+    infod = info.json()
+    todod = todo.json()
 
-infod = info.json()
-todod = todo.json()
+    name = infod.get('name')
+    tasks = len(todod)
 
-name = infod.get('name')
-tasks = len(todod)
+    count = 0
+    for comp in todod:
+        finished = comp.get('completed')
+        if finished:
+            count += 1
 
-count = 0
-for comp in todod:
-    finished = comp.get('completed')
-    if finished:
-        count += 1
-
-print('Employee {} is done with tasks({}/{}):'.format(name, count, tasks))
-for task in todod:
-    title = task.get('title')
-    print("\t {}".format(title))
+    print('Employee {} is done with tasks({}/{}):'.format(name, count, tasks))
+    for task in todod:
+        title = task.get('title')
+        print("\t {}".format(title))
