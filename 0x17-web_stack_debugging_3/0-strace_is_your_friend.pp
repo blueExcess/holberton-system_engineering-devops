287494dcacb8c3cfb2a7a94540f61d6a59c85ade
Nate Petersen <nate.petersen@me.com>
2019-10-03 01:47:19 +0000

    remove 'path' command from puppet script

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 # 0. Use Puppet to run SED to change ".phpp" to ".php"
 exec { "type fixer ftw":
-          command => '/bin/sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
-	  path => '/bin'
+          command => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
 }
