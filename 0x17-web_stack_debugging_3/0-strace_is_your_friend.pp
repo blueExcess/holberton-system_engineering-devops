9d1781e10b25063c95386ea165d1b58e73c1eb85
Nate Petersen <nate.petersen@me.com>
2019-10-03 02:08:24 +0000

    fix puppet-lint indent issue

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # 0. Use Puppet to run SED to change ".phpp" to ".php"
 exec { 'type fixer ftw':
-    command => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
+    command  => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
     provider => shell,
 }
